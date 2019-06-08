4b72d47f59898abea1402968adabeb4723b82de3
JOYAL RAJU <51167856+joyalraju@users.noreply.github.com>
2019-06-07 23:50:17 -0400

    function for switch condition

---
+++ choices.m
@@ -0,0 +1,17 @@
+
+function ch = choices()
+  val1= input('enter the number: ');
+  switch val1
+    case 1 
+      ch = 'choice 1';
+  case 2
+    ch = ' choice 2';
+case 3
+  ch= 'choice 3';
+otherwise 
+ ch = 'Error choice';
+ end 
+  
+endfunction
+
+
